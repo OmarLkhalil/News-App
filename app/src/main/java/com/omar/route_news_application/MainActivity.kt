5f70a02764f2dfd9ff6aede1542dbf24065984a7
Omar Mohamed <98606902+OmarLkhalil@users.noreply.github.com>
2022-11-09 00:22:58 +0200

    Started project

---
+++ app/src/main/java/com/omar/route_news_application/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.omar.route_news_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
